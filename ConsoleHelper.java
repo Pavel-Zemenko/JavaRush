1bad0edbf107deaa24afb12e499ca1547b824c48
Pavel <pavel_zemenko@ukr.net>
2020-09-10 01:05:58 +0300

    CashMachine (2)

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,4 +1,22 @@
 package cashmachine;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String string = "";
+        try {
+            string = reader.readLine();
+        } catch (IOException e) {
+        }
+        return string;
+    }
 }
